9f0b14350312a540f89fc58b2b4ff49fa18ea9d5
Bast <bastien.last87@gmail.com>
2020-04-01 12:11:45 +0200

    Ajout de l'insertion dans la base de données

--- paull.js
+++ paull.js
@@ -19,12 +19,21 @@ client.on("message", async (message) => {
 
   if (isNotACommand(message)) return;
 
-  let args = message.content.slice(prefix.length).trim().split(/ +/g);
-  if (isANumberPoll(args)) {
-    await reactWithNumber(args, message);
-  } else {
-    await reactWithYesNo(message);
+  let args = getArgs(message);
+  if (argsIsNotValid(args)) {
+    return sendArgsErrorMessage(message)
   }
+  if (args[1] == undefined) {
+    return sendQuestionErrorMessage(message)
+  }
+  let question = getQuestion(message, args);
+  let msg = await message.channel.send(question);
+  if (isANumberPoll(args)) {
+    await reactWithNumber(args, msg);
+  } else {
+    await reactWithYesNo(msg);
+  }
+  sql.run(`INSERT INTO poll (messageId, time, numberOfOptions, one, two, three, four, five, six, seven, eight, nine, ten) VALUES (${message.id},${message.createdTimestamp},${args[0]},0,0,0,0,0,0,0,0,0,0) `).catch(console.error);
 });
 
 
@@ -32,6 +41,51 @@ client.on("messageReactionAdd", async (reaction) => {
 
 });
 
+/**
+ * get the question in a string
+ * @param {*} message 
+ * @param {*} args 
+ */
+function getQuestion(message, args) {
+  return message.content.slice(prefix.length).trim().slice(args[0].length);
+}
+
+/**
+ * get an array of the args of the command
+ * @param {*} message 
+ */
+function getArgs(message) {
+  return message.content.slice(prefix.length).trim().split(/ +/g);
+}
+
+/**
+ * display an error
+ * @param {*} message 
+ */
+function sendArgsErrorMessage(message) {
+  return message.channel.send(":x: Veuillez choisir un nombre d'options compris entre 2 et 10 : `?s [nombre d'option] Question`");
+}
+
+/**
+ * display an error
+ * @param {*} message 
+ */
+function sendQuestionErrorMessage(message) {
+  return message.channel.send(":x: Veuillez indiquer une question : `?s [nombre d'option] Question`");
+}
+
+/**
+ * test the validitu of the args
+ * @param {*} args 
+ */
+function argsIsNotValid(args) {
+  return args[0] < 2 || args[0] > 10;
+}
+
+/**
+ * test if the bot has a two choice poll
+ * @param {*} message 
+ */
 async function reactWithYesNo(message) {
   await message.react("✅"),
     message.react("❌");
@@ -65,7 +119,7 @@ async function reactWithNumber(args, message) {
  * @param {*} args 
  */
 function isANumberPoll(args) {
-  return args[0] < 11;
+  return args[0] < 11 && args[0] > 2;
 }
 
 /**
@@ -75,9 +129,9 @@ function isANumberPoll(args) {
 function createDatabase(sql) {
   //first check if the database is not already there
   sql.get(`SELECT version FROM database`).catch(() => {
-    sql.run("CREATE TABLE IF NOT EXISTS poll (messageId TEXT, time INTEGER, numberOfOptions INTEGER, one INTEGER, two INTEGER, three INTEGER, four INTEGER, five INTEGER, six INTEGER, seven INTEGER, eight INTEGER, nine INTEGER, ten INTEGER)").catch(console.error);
+    sql.run("CREATE TABLE IF NOT EXISTS poll (messageId INTEGER, time INTEGER, numberOfOptions INTEGER, one INTEGER, two INTEGER, three INTEGER, four INTEGER, five INTEGER, six INTEGER, seven INTEGER, eight INTEGER, nine INTEGER, ten INTEGER)").catch(console.error);
     sql.run("CREATE TABLE IF NOT EXISTS database (version TEXT, lastReset INTEGER)").then(() => {
-      sql.run(`INSERT INTO database (version, lastReset) VALUES (\"${Config.version}\",0)`).then(() => {
+      sql.run(`INSERT INTO database (version) VALUES (1)`).then(() => {
         console.log("... Generation Complete !");
       });
     });
